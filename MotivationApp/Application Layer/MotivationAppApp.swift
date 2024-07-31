45213a7d964e2ab332d8503fdce553982b588c24
techleadtanir <techleadmytaxi@gmail.com>
2024-07-31 11:39:08 +0500

    feat: Configure app and folders

---
+++ MotivationApp/Application Layer/MotivationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationAppApp.swift
+//  MotivationApp
+//
+//  Created by Tanirbergen Kaldibai on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MotivationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivationApp/Buiness Logic Layer/MainViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  MotivationApp
+//
+//  Created by Tanirbergen Kaldibai on 31.07.2024.
+//
+
+import Foundation
+
+struct MainViewModel {
+    var name: String
+}
